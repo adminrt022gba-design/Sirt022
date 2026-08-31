feaabdc5dcdefff87d0ab110eb4d74d7dcc9bbad
adminrt022gba-design <admin.rt022gba@gmail.com>
2026-08-31 20:49:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
-const SUPABASE_URL = "https://kwjadchayfyxvgudmcv.supabase.co";
+// ========================================
+// KONFIGURASI SIRT 022
+// ========================================
 
-const SUPABASE_ANON_KEY = "sb_publishable_i7P5jTWsiN7xos0YL0sKcA_BA_gMc8A";
+// Alamat utama project Supabase.
+// Jangan tambahkan /rest/v1/ di belakang.
+const SUPABASE_URL =
+  "https://kwjadachayfyxvgudmcv.supabase.co";
+
+// Publishable Key untuk aplikasi website.
+// Jangan gunakan service_role atau sb_secret.
+const SUPABASE_ANON_KEY =
+  "sb_publishable_i7P5jTWsiN7xos0YL0sKcA_BA_gMc8A";
